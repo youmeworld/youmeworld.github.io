b502ea37ae730620b0039628c88990e4ef1e60b8
Kameron Robinson <kameron.robinson@krobinson-m.jet.local>
2017-11-02 17:16:05 -0400

    Updates

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-// https://github.com/tkh44/emotion-create-react-app-example/blob/master/config-overrides.js
-const { injectBabelPlugin } = require('react-app-rewired');
-
-module.exports = function override(config, env) {
-  config = injectBabelPlugin('emotion', config);
-  return config;
-};
--- src/components/Landing/story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Landing from './';
-
-storiesOf('Landing', module)
-	.add('base', () =>
-		<Landing>Component</Landing>
-	);
